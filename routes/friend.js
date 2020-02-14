8fcf022deb646f3d6656629e8ddf4a10c404886e
Chau Do <c6do@ucsd.edu>
2020-02-13 18:46:11 -0800

    change found in friend.js

--- routes/friend.js
+++ routes/friend.js
@@ -4,12 +4,12 @@ var total_events = require("../data/total.json");
 exports.list = function(req, res) {
   var friend = req.params.friend;
   var friends_arr = data.friends;
-  var found = {};
+  var found;
 
   // look for a specific friend
   for (var i = 0; i < friends_arr.length; i++) {
     if (friends_arr[i].name === friend) {
-      found = { ...friends_arr[i] };
+      found = friends_arr[i];
     }
   }
 
