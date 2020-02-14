dd3231c21afa957571467602ee8341848594d6ee
Chau Do <c6do@ucsd.edu>
2020-02-13 17:12:58 -0800

    fix friend.js

--- app.js
+++ app.js
@@ -19,11 +19,11 @@ var letter = require("./routes/letter");
 var app = express();
 
 // all environments
-app.set("port", process.env.PORT || 3000);
+app.set("port", process.env.PORT || 5000);
 app.set("views", path.join(__dirname, "views"));
 app.engine("handlebars", handlebars());
 app.set("view engine", "handlebars");
-// app.use(express.favicon());
+app.use(express.favicon());
 app.use(express.logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded());
--- routes/friend.js
+++ routes/friend.js
@@ -1,15 +1,26 @@
 var data = require("../data/friends.json");
+var total_events = require("../data/total.json");
 
 exports.list = function(req, res) {
   var friend = req.params.friend;
   var friends_arr = data.friends;
-  var found;
+  var found = {};
 
   // look for a specific friend
   for (var i = 0; i < friends_arr.length; i++) {
     if (friends_arr[i].name === friend) {
-      found = friends_arr[i];
+      found = { ...friends_arr[i] };
     }
+  }
+
+  // if found the friend, push all activities of friends
+  // into a new array to be rendered
+  if (found) {
+    var activites = total_events.events.filter(
+      event => event.weeth === found.name
+    );
+    found.activities = [];
+    found.activities.push(...activites);
   }
   console.log(found);
   res.render("friend", found);
